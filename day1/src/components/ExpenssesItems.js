11558c6eb156101a6b9b27f5ff3b8dbd9752b91a
kunalSinghQA <kunal.s@feuji.com>
2023-04-06 00:27:09 +0530

    day 2 scripts updated

--- day1/src/components/ExpenssesItems.js
+++ day1/src/components/ExpenssesItems.js
@@ -1,5 +1,19 @@
+import "../style/ExpenseItems.css"
+
 function ExpenseItems() {
-    return <h1> ExpenseItems </h1>
+  return (
+    <div>
+      <div className="row float-center">
+        <div className="col text-primary expenseItem">06th April 2023</div>
+        <div className="col expenseItemDescription">
+          <div className="row">
+            <div className="col text-primary">Car Insurance</div>
+            <div className="col text-primary">$292.64</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItems;
